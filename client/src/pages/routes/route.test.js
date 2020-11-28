ede9b68838ea455c19a9cf739782e5392eddf140
Amin Mohammed-Coleman <amin@digitalpatterns.io>
2020-11-28 19:47:39 +0100

    placeholder work

--- client/src/pages/routes/route.test.js
+++ client/src/pages/routes/route.test.js
@@ -18,17 +18,17 @@ describe('Base routes', () => {
     ${'/login?redirectTo=/'}        | ${true}
     ${'/logout'}                    | ${true}
   `('title and views for $path should be defined', async ({ path, authenticated }) => {
-    const navigation = createMemoryNavigation({
-      url: `${path}`,
-      routes,
-      context: {
-        t: (key) => key,
-        isAuthenticated: authenticated,
-      },
-    });
-    const route = await navigation.getRoute();
-    expect(route.error).toBeUndefined();
-    expect(route.views).toBeDefined();
-    expect(route.title).toBeDefined();
+  const navigation = createMemoryNavigation({
+    url: `${path}`,
+    routes,
+    context: {
+      t: (key) => key,
+      isAuthenticated: authenticated,
+    },
   });
+  const route = await navigation.getRoute();
+  expect(route.error).toBeUndefined();
+  expect(route.views).toBeDefined();
+  expect(route.title).toBeDefined();
+});
 });
